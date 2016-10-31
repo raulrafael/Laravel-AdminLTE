722414f42262de0280caf406719962b5c4da95f5
Raul <raul.sosa@outlook.com>
2016-10-30 20:33:26 -0600

    0.0.2.1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,10 +11,14 @@
 			<div class="col-md-10 col-md-offset-1">
 				<div class="panel panel-default">
 					<div class="panel-heading">Home</div>
-
+		
 					<div class="panel-body">
 						{{ trans('adminlte_lang::message.logged') }}
 					</div>
+
+					<p>  Apartado Body
+							Sistemas Expertos: Este curso desarrolla los avances obtenidos en el área de inteligencia artificial y en definitiva de los sistemas expertos. El curso se desarrolla alrededor de múltiples disciplinas como: ciencias de la computación, ingeniería, medicina, matemáticas, economía y ciencias sociales.
+					</p>
 				</div>
 			</div>
 		</div>
